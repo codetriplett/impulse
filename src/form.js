c37ff6dcd3582b3c659ab42b14fac8e8c6f0eb8a
Jeff Triplett <jeffrey.v.triplett@gmail.com>
2025-02-16 22:51:43 -0600

    form objects and arrays

--- src/form.js
+++ src/form.js
@@ -1,54 +1,185 @@
-export function FormField (value, ...names) {
-	if (Array.isArray(value)) {
-		const [first, ...rest] = value;
-		const field = FormField(first, ...names);
+// TODO: decide if this is needed
+// - The built-in form validation should be enough
+// - When populating form, cast all non-objects to strings
+//   - for references, check that the path matches in the '' prop
+//   - for objects given as options, check the '' prop for its id (required when more than one object options is in schema)
+export function checkInput (input, value) {
+	const { type, min, max, pattern, minlength, maxlength } = input[1];
 
-		if (field[0][1].type === 'checkbox') {
-			const [, label] = field;
-			const id = label[1].for
-			const select = ['select', { id }];
+	if (type === 'number') {
+		return (min === undefined || value >= min) && (max === undefined || value <= max);
+	}
+}
 
-			for (const value of rest) {
-				const [label, input] = FormField(value);
-				const { type, placeholder } = input[1];
-
-				if (!placeholder) {
-					continue;
-				}
-
-				const props = { value: placeholder };
-
-				if (type === 'number') {
-					props.dataset = { type };
-				}
-
-				const option = ['option', props, label[2]];
-				select.push(option);
+function parseTypes (value) {
+	switch (typeof value) {
+		case 'boolean': {
+			return ['checkbox'];
+		}
+		case 'number': {
+			return ['number', 'range']
+		}
+		case 'string': {
+			if (/^\d+-\d\d-\d\dT\d\d:\d\d$/.test(value)) {
+				return ['datetime-local'];
+			} else if (/^\d+-\d\d-\d\d$/.test(value)) {
+				return ['date'];
 			}
 
-			return [label, select];
+			break;
+		}
+	}
+
+	return [];
+}
+
+// TODO: work out way to allow neseted option/array/object definitions
+// - for now, only simple inputs are allowed
+export function findOption (value, ...options) {
+	if (value === undefined) {
+		return -1;
+	} else if (Array.isArray(value)) {
+		// look for first array it finds
+		// - too complicated to test each value of each one to find the best match
+		return;
+	}
+
+	let path;
+
+	if (typeof value === 'object') {
+		// TODO: also load schema of matched component and fill in overrides
+		const { '': url, ...overrides } = value;
+
+		if (url === undefined) {
+			// look for first object it finds without an id (otherwise first one with id)
+			// - too complicated to test each property of each one to find the best match
+			return;
+		} else if (url[0] !== '/') {
+			// look for first object it finds that matches the id (otherwise first one without an id)
+			return;
+		}
+
+		const lastSlashIndex = url.lastIndexOf('/');
+		path = url.slice(0, lastSlashIndex + 1);
+		value = url.slice(lastSlashIndex);
+	}
+
+	const types = parseTypes(value);
+
+	const filteredOptions = options.filter(option => {
+		const [tagName, { type, dataset }] = option;
+		return (types.indexOf(type) !== -1 || type === 'text' || tagName === 'textarea') && dataset?.path === path;
+	});
+
+	// TODO: find first one that matches the min/max or pattern requirements (otherwise choose the first one)
+
+	const option = filteredOptions[0];
+	return options.indexOf(option);
+}
+
+export function FormField (definition, value, ...names) {
+	if (Array.isArray(definition)) {
+		const [first, ...rest] = definition;
+		const field = FormField(first, undefined, ...names);
+		const inputs = [];
+
+		if (field[1]?.[0] === 'label') {
+			field.reverse();
+		}
+
+		const options = rest.map(definition => {
+			const [label, input] = FormField(definition);
+			const { type, placeholder, disabled } = input?.[1] || {};
+			const props = {};
+
+			if (disabled) {
+				props.value = placeholder;
+
+				if (type === 'number' || type === 'range') {
+					props.dataset = { type: /^true|false$/.test(placeholder) ? 'boolean' : 'number' };
+				}
+			}
+
+			inputs.push(input);
+			return ['option', props, label[2]];
+		});
+
+		const [label, input] = field;
+		const select = ['select', {}, ...options];
+
+		switch (input?.[1]?.type) {
+			case 'checkbox': {
+				const index = findOption(value, ...inputs);
+
+				if (index !== -1) {
+					const input = inputs[index];
+					options[index][1].selected = true;
+
+					if (!input[1].disabled) {
+						const field = FormField(rest[index], value, ...names);
+						const input = field[field[0][0] === 'label' ? 1 : 0];
+						return [label, select, input];
+					}
+				} else {
+					select[1].id = label[1].for;
+				}
+
+				return [label, select];
+			}
+			case 'number':
+			case 'range': {
+				const array = Array.isArray(value) ? value : [];
+				const list = ['ol', {}];
+				label[1] = {};
+				select[1].dataset = { type: 'array' };
+
+				for (const [i, value] of array.entries()) {
+					const index = findOption(value, ...inputs);
+					const item = ['li', {}];
+
+					if (index === -1) {
+						item[2] = 'Invalid Item';
+					} else {
+						const field = FormField(rest[index], value, ...names, i);
+						item[2] = field[field[0][0] === 'label' ? 1 : 0];
+					}
+
+					list.push(item);
+				}
+
+				const option = ['option', {}, 'Add new item...'];
+				select.splice(2, 0, option);
+				return [label, list, select];
+			}
+			default: {
+				// TODO: similar to above, but set dataset: { type: 'object' }
+				// - also put input element from definition before select element
+				break;
+			}
 		}
 
 		// TODO: add select box with onchange that inserts the correct field(s)
 		// - look at cms project for existing code
-		return;
-	} else if (typeof value === 'object') {
+		return [];
+	} else if (typeof definition === 'object') {
+		const object = typeof value === 'object' && !Array.isArray(value) ? value : {};
 		const elements = [];
 
-		for (const [name, value] of Object.entries(value)) {
-			elements.push(...FormField(value, ...names, name));
+		for (const [name, value] of Object.entries(definition)) {
+			elements.push(...FormField(value, object[name], ...names, name));
 		}
 
 		return elements;
 	}
 
-	let match = value.match(/^\s*(?:(.+?)\s+)?(.*?)\/(.*?)(?:\/([^/]*?))?(?:\/([^/]*?))?(?:\s+(.+))?\s*$/);
+	let match = definition.match(/^\s*(?:(.+?)\s+)?(\S*?)\/(.*?)(?:\/([^/]*?))?(?:\/([^/]*?))?(\*?)?(?:\s+(.+))?\s*$/);
 
 	if (!match) {
-		match = [,, 'checkbox',,,, value.trim()];
+		match = [,, 'checkbox', '',,,, definition.trim()];
 	}
 
-	let [, placeholder, type, path, pattern, range, text] = match;
+	let [, placeholder, type, path, pattern, range, required, text] = match;
+	const isLiteral = type === '*';
 
 	if (pattern === undefined) {
 		range = path;
@@ -59,25 +190,51 @@ export function FormField (value, ...names) {
 		path = undefined;
 	}
 
-	if (!type) {
-		type = pattern !== undefined ? 'text' : range.indexOf('T') !== -1 ? 'datetime-local' : range.indexOf('-') !== -1 ? 'date' : 'number';
+	if (!type || isLiteral) {
+		type = pattern !== undefined ? 'text' : 'number';
 	}
 
-	const id = names.join('.');
-	const props = { id, type };
+	const id = names.reduce((id, name) => `${id}${typeof name === 'number' ? `[${name}]` : `${id ? '.' : ''}${name}`}`, '');
+	const props = { id };
 	const label = ['label', { for: id }, text];
 	const input = ['input', props];
 
-	if (type === 'checkbox') {
-		return [input, label];
+	if ((value || value === 0) && typeof value !== 'object') {
+		if (type === 'textarea') {
+			input[2] = String(value);
+		} else if (type !== 'checkbox') {
+			props.value = String(value);
+		}
+	}
+
+	if (type === 'textarea') {
+		input[0] = 'textarea';
+	} else {
+		props.type = type;
+	}
+
+	if (isLiteral) {
+		props.disabled = true;
 	}
 
 	if (placeholder) {
 		props.placeholder = placeholder;
 	}
 
+	if (type === 'checkbox') {
+		if (value === true) {
+			input[1].checked = true;
+		}
+
+		return [input, label];
+	}
+
 	if (pattern) {
 		props.pattern = pattern;
+	}
+
+	if (required) {
+		props.required = true;
 	}
 
 	if (range) {
--- src/form.test.js
+++ src/form.test.js
@@ -1,4 +1,4 @@
-import { FormField } from './form';
+import { checkInput, findOption, FormField } from './form';
 
 /*
 
@@ -20,9 +20,63 @@ path/path/\w+/
 
 */
 
+describe('checkInput', () => {
+	it('accepts number within free range', () => {
+		const actual = checkInput([, { type: 'number' }], 2);
+		expect(actual).toEqual(true);
+	});
+
+	it('accepts number within strict range', () => {
+		const actual = checkInput([, { type: 'number', min: 0, max: 4 }], 2);
+		expect(actual).toEqual(true);
+	});
+	
+	it('rejects number below range', () => {
+		const actual = checkInput([, { type: 'number', min: 2, max: 4 }], 0);
+		expect(actual).toEqual(false);
+	});
+	
+	it('rejects number above range', () => {
+		const actual = checkInput([, { type: 'number', min: 0, max: 2 }], 4);
+		expect(actual).toEqual(false);
+	});
+});
+
+describe('findOption', () => {
+	it('finds checkbox input', () => {
+		const actual = findOption(true, [, {}], [, { type: 'checkbox' }]);
+		expect(actual).toEqual(1);
+	});
+
+	it('finds number input', () => {
+		const actual = findOption(123, [, {}], [, { type: 'number' }]);
+		expect(actual).toEqual(1);
+	});
+
+	it('finds text input', () => {
+		const actual = findOption('abc', [, {}], [, { type: 'text' }]);
+		expect(actual).toEqual(1);
+	});
+
+	it('finds textarea input', () => {
+		const actual = findOption('abc', [, {}], ['textarea', {}]);
+		expect(actual).toEqual(1);
+	});
+
+	it('finds reference input', () => {
+		const actual = findOption({ '': '/folder/file' }, [, {}], [, { type: 'text', dataset: { path: '/folder/' }}]);
+		expect(actual).toEqual(1);
+	});
+
+	it('rejects undefined', () => {
+		const actual = findOption(undefined, [, {}], [, { type: 'text' }]);
+		expect(actual).toEqual(-1);
+	});
+});
+
 describe('FormField', () => {
 	it('checkbox', () => {
-		const actual = FormField('Label', 'group', 'name');
+		const actual = FormField('Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['input', { type: 'checkbox', id: 'group.name' }],
@@ -30,8 +84,17 @@ describe('FormField', () => {
 		]);
 	});
 
+	it('checkbox populated', () => {
+		const actual = FormField('Label', true, 'group', 'name');
+
+		expect(actual).toEqual([
+			['input', { type: 'checkbox', id: 'group.name', checked: true }],
+			['label', { for: 'group.name' }, 'Label'],
+		]);
+	});
+
 	it('number', () => {
-		const actual = FormField('/ Label', 'group', 'name');
+		const actual = FormField('/ Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -39,8 +102,17 @@ describe('FormField', () => {
 		]);
 	});
 
+	it('number populated', () => {
+		const actual = FormField('/ Label', 123, 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['input', { type: 'number', id: 'group.name', value: '123' }],
+		]);
+	});
+
 	it('number with range longhand', () => {
-		const actual = FormField('/.. Label', 'group', 'name');
+		const actual = FormField('/.. Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -49,7 +121,7 @@ describe('FormField', () => {
 	});
 
 	it('number with max shorthand', () => {
-		const actual = FormField('/4 Label', 'group', 'name');
+		const actual = FormField('/4 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -58,7 +130,7 @@ describe('FormField', () => {
 	});
 
 	it('number with max', () => {
-		const actual = FormField('/..4 Label', 'group', 'name');
+		const actual = FormField('/..4 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -67,7 +139,7 @@ describe('FormField', () => {
 	});
 
 	it('number with min', () => {
-		const actual = FormField('/0.. Label', 'group', 'name');
+		const actual = FormField('/0.. Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -76,7 +148,7 @@ describe('FormField', () => {
 	});
 
 	it('number with step', () => {
-		const actual = FormField('/..2.. Label', 'group', 'name');
+		const actual = FormField('/..2.. Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -85,7 +157,7 @@ describe('FormField', () => {
 	});
 
 	it('number with min and max', () => {
-		const actual = FormField('/0..4 Label', 'group', 'name');
+		const actual = FormField('/0..4 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -94,7 +166,7 @@ describe('FormField', () => {
 	});
 
 	it('number with min and step', () => {
-		const actual = FormField('/0..2.. Label', 'group', 'name');
+		const actual = FormField('/0..2.. Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -103,7 +175,7 @@ describe('FormField', () => {
 	});
 
 	it('number with step and max', () => {
-		const actual = FormField('/..2..4 Label', 'group', 'name');
+		const actual = FormField('/..2..4 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -112,7 +184,7 @@ describe('FormField', () => {
 	});
 
 	it('number with min, step, and max', () => {
-		const actual = FormField('/0..2..4 Label', 'group', 'name');
+		const actual = FormField('/0..2..4 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -121,7 +193,7 @@ describe('FormField', () => {
 	});
 
 	it('custom number type', () => {
-		const actual = FormField('date/2000-01-01..7..2020-01-01 Label', 'group', 'name');
+		const actual = FormField('date/2000-01-01..7..2020-01-01 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -130,7 +202,7 @@ describe('FormField', () => {
 	});
 
 	it('custom number type reference number', () => {
-		const actual = FormField('date/path//2020-01-01 Label', 'group', 'name');
+		const actual = FormField('date/path//2020-01-01 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -139,7 +211,7 @@ describe('FormField', () => {
 	});
 
 	it('text', () => {
-		const actual = FormField('// Label', 'group', 'name');
+		const actual = FormField('// Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -147,8 +219,17 @@ describe('FormField', () => {
 		]);
 	});
 
+	it('text populated', () => {
+		const actual = FormField('// Label', 'abc', 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['input', { type: 'text', id: 'group.name', value: 'abc' }],
+		]);
+	});
+
 	it('text with pattern', () => {
-		const actual = FormField('/\\w*/ Label', 'group', 'name');
+		const actual = FormField('/\\w*/ Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -157,16 +238,34 @@ describe('FormField', () => {
 	});
 
 	it('text with maxlength', () => {
-		const actual = FormField('//4 Label', 'group', 'name');
+		const actual = FormField('//4 Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
 			['input', { type: 'text', id: 'group.name', maxlength: '4' }],
 		]);
 	});
+
+	it('textarea', () => {
+		const actual = FormField('textarea// Label', undefined, 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['textarea', { id: 'group.name' }],
+		]);
+	});
+
+	it('textarea populated', () => {
+		const actual = FormField('textarea// Label', 'abc', 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['textarea', { id: 'group.name' }, 'abc'],
+		]);
+	});
 	
 	it('reference', () => {
-		const actual = FormField('/path/\\w*/ Label', 'group', 'name');
+		const actual = FormField('/path/\\w*/ Label', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Label'],
@@ -174,8 +273,8 @@ describe('FormField', () => {
 		]);
 	});
 
-	it.skip('sets placeholder', () => {
-		const actual = FormField('Enter Value // Field Name', 'group', 'name');
+	it('sets placeholder', () => {
+		const actual = FormField('Enter Value // Field Name', undefined, 'group', 'name');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.name' }, 'Field Name'],
@@ -183,11 +282,20 @@ describe('FormField', () => {
 		]);
 	});
 
+	it('sets as required', () => {
+		const actual = FormField('//* Label', undefined, 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['input', { type: 'text', id: 'group.name', required: true }],
+		]);
+	});
+
 	it('object', () => {
 		const actual = FormField({
 			number: '/ Number',
 			text: '// Text',
-		}, 'group');
+		}, undefined, 'group');
 
 		expect(actual).toEqual([
 			['label', { for: 'group.number' }, 'Number'],
@@ -197,15 +305,122 @@ describe('FormField', () => {
 		]);
 	});
 
-	it('option', () => {
+	it('object populated', () => {
+		const actual = FormField({
+			number: '/ Number',
+			text: '// Text',
+		}, {
+			number: 123,
+			text: 'abc',
+		}, 'group');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.number' }, 'Number'],
+			['input', { type: 'number', id: 'group.number', value: '123' }, ],
+			['label', { for: 'group.text' }, 'Text'],
+			['input', { type: 'text', id: 'group.text', value: 'abc' }],
+		]);
+	});
+
+	// 'Label': checkbox
+	// ['Label', ...]: radio buttons showing each option
+	// - display form options if non-literal is selected
+
+	// '/ Label': number input
+	// ['/ Label', ...]: select box where selection creates a new array item
+
+	// '// Label': string input
+	// ['// Label', ...]: select box where selection creates a new property in an object
+	// - key must stick to pattern and range
+	// - not sure what the use would be for this, but it makes the most sense for the format
+	// - essentially, the type sets the key format of object/array
+
+	it('select', () => {
 		const actual = FormField(['Label',
+			'123 */ Number',
+			'abc *// Text',
+		], undefined, 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['select', { id: 'group.name' },
+				['option', { value: '123', dataset: { type: 'number' } }, 'Number'],
+				['option', { value: 'abc' }, 'Text'],
+			],
+		]);
+	});
+
+	// TODO: what ids do we set when an option isn't a literal
+	// - we can put the extra fields below the select, but would have to clear the id from teh sle
+	it('select populated', () => {
+		const actual = FormField(['Label',
+			'/ Number',
+			'// Text',
+		], 'abc', 'group', 'name');
+
+		expect(actual).toEqual([
+			['label', { for: 'group.name' }, 'Label'],
+			['select', {},
+				['option', {}, 'Number'],
+				['option', { selected: true }, 'Text'],
+			],
+			['input', { type: 'text', id: 'group.name', value: 'abc'}]
+		]);
+	});
+
+	it('array builder', () => {
+		const actual = FormField(['/ Label',
+			'/ Number',
+			'// Text',
+		], undefined, 'group');
+
+		expect(actual).toEqual([
+			['label', {}, 'Label'],
+			['ol', {}],
+			['select', { dataset: { type: 'array' } },
+				['option', {}, 'Add new item...'],
+				['option', {}, 'Number'],
+				['option', {}, 'Text'],
+			],
+		]);
+	});
+
+	it('array builder populated', () => {
+		const actual = FormField(['/ Label',
+			'/ Number',
+			'// Text',
+		], [123, 'abc'], 'group');
+
+		expect(actual).toEqual([
+			['label', {}, 'Label'],
+			['ol', {},
+				['li', {},
+					['input', { type: 'number', id: 'group[0]', value: '123' }],
+				],
+				['li', {},
+					['input', { type: 'text', id: 'group[1]', value: 'abc' }],
+				],
+			],
+			['select', { dataset: { type: 'array' } },
+				['option', {}, 'Add new item...'],
+				['option', {}, 'Number'],
+				['option', {}, 'Text'],
+			],
+		]);
+	});
+
+	it.skip('object builder', () => {
+		const actual = FormField(['// Label',
+			'true / Boolean',
 			'123 / Number',
 			'abc // Text',
 		], 'group', 'name');
 
 		expect(actual).toEqual([
-			['label', { for: 'group.name' }, 'Label'],
-			['select', { id: 'group.name'},
+			['label', {}, 'Label'],
+			['select', { dataset: { type: 'object' }, onchange: expect.any(Function) },
+				['option', {}, 'Add new item...'],
+				['option', { value: 'true', dataset: { type: 'boolean' } }, 'Boolean'],
 				['option', { value: '123', dataset: { type: 'number' } }, 'Number'],
 				['option', { value: 'abc' }, 'Text'],
 			],
